2c95afb2cd67ee8c8d1f4871d50b6a34bc928436
jumajo <julemanzon@gmail.com>
2021-05-06 01:29:55 +0000

    W5D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
@@ -9,21 +15,17 @@ get '/signup' do
 end
 
 post '/signup' do
-
   email      = params[:email]
   avatar_url = params[:avatar_url]
   username   = params[:username]
   password   = params[:password]
 
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
   if @user.save
-
-    "User #{username} saved!"
-
+    redirect to('/login')
   else
-
     erb(:signup)
-
   end
 end
 
@@ -32,20 +34,37 @@ get '/login' do
 end
 
 post '/login' do
+  username = params[:username]
+  password = params[:password]
 
-  email      = params[:email]
-  avatar_url = params[:avatar_url]
-  username   = params[:username]
-  password   = params[:password]
-  
-  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
-  if @user.save
-  
-    "User #{username} saved!"
-  
+  @user = User.find_by(username: username)
+
+  if @user && @user.password == password
+    session[:user_id] = @user.id
+    redirect to('/')
   else
-  
-    erb(:signup)
-  
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
 
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
 
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+  
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
