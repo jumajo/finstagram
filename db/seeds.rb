dd2e02dcb65b7a7537818311d81c75cf3ab25bc8
jumajo <julemanzon@gmail.com>
2021-05-14 00:24:13 +0000

    created seeds file for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,9 @@ user_peppa.save
 
 finstagram_post_peppa = FinstagramPost.new({ photo_url: "http://naserca.com/images/marlin.jpg", user_id: user_peppa.id })
 finstagram_post_peppa.save
+
+user_peppa = User.new({ username: "jumajo", avatar_url: "https://cdn.britannica.com/94/101794-050-A98A953E/Lighthouse-Portsmouth-NH.jpg", email: "julemanzon@gmail.com", password: "abcd" })
+user_peppa.save
+
+finstagram_post_peppa = FinstagramPost.new({ photo_url: "https://media.gq.com/photos/5da62c0d49acb20008f2501c/4:3/w_2699,h_2024,c_limit/the-lighthouse-film-review-gq-october-101519.jpg", user_id: user_peppa.id })
+finstagram_post_peppa.save
